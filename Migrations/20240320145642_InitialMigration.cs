78944077077315c090489fc2b8405d8518a9fe36
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-03-20 12:01:15 -0300

    add migration

---
+++ Migrations/20240320145642_InitialMigration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharpCodingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: false),
+                    Saldo = table.Column<decimal>(type: "TEXT", nullable: true),
+                    IsLogado = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    AtualizadoEm = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    DeletadoEm = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    NumeroConta = table.Column<int>(type: "INTEGER", nullable: false),
+                    Agencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Historico = table.Column<string>(type: "TEXT", nullable: false),
+                    DirecaoTransacao = table.Column<int>(type: "INTEGER", nullable: false),
+                    TipoTransacao = table.Column<int>(type: "INTEGER", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumeroContaDestino = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumeroContaOrigem = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_UserId",
+                table: "Transacoes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using SharpCodingAPI.Domains.DTO.dbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(); 
+builder.Services.AddControllers(); // Registra e Procura controladores na aplicacao
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BankDbContext>(options => {
+    options.UseSqlite(defaultConnectionString);
+});
+
+builder.Services.AddCors(options =>
+{
+   options.AddDefaultPolicy(
+     policy =>
+     {
+       policy.AllowAnyOrigin()
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+     });
+});
 
 var app = builder.Build();
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
 }
 
 app.UseHttpsRedirection();
 
-app.MapControllers();
+
+app.MapControllers(); // mapeia os controladores quando a aplicacao iniciar
 
 app.Run();
 
